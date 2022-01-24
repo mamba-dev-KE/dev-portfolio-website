c1ad1652d264ebcbdd941358e177f9b1e9144f97
Joseph Maramba <josephmaramba50@gmail.com>
2022-01-24 14:18:30 +0300

    header styles

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,16 +3,19 @@ import * as headerStyles from "./header.module.css";
 
 const Header = () => {
 	return (
-		<header>
-			<nav>
-				<ul className={headerStyles.color}>
-					<li>About</li>
-					<li>Skills</li>
-					<li>Projects</li>
-					<li>Contact</li>
-					<li>Resume</li>
-				</ul>
-			</nav>
+		<header className={headerStyles.header}>
+			<div className={headerStyles.container}>
+				<nav className={headerStyles.nav}>
+					<h4 className={headerStyles.logo}>Joseph</h4>
+					<ul className={headerStyles.nav_list}>
+						<li className={headerStyles.nav_list_item}>About</li>
+						<li className={headerStyles.nav_list_item}>Skills</li>
+						<li className={headerStyles.nav_list_item}>Projects</li>
+						<li className={headerStyles.nav_list_item}>Contact</li>
+						<li className={headerStyles.nav_list_item}>Resume</li>
+					</ul>
+				</nav>
+			</div>
 		</header>
 	);
 };
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,12 @@
 import * as React from "react";
 import Layout from "../components/Layout/Layout";
+import Skills from "../components/Skills/Skills";
 
 const IndexPage = () => {
 	return (
 		<Layout>
 			<h1>Index Page</h1>
+			<Skills />
 		</Layout>
 	);
 };
