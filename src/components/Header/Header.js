62009d7b55af8814554fa61b5bd62c9b85a8fdf4
Joseph Maramba <josephmaramba50@gmail.com>
2022-01-24 14:33:32 +0300

    header responsiveness implementation

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,19 +1,38 @@
 import * as React from "react";
+import { useState, useEffect } from "react";
 import * as headerStyles from "./header.module.css";
+import MenuIcon from "@mui/icons-material/Menu";
 
 const Header = () => {
+	const [width, setWidth] = useState(undefined);
+
+	useEffect(() => {
+		const handleResize = () => {
+			setWidth(window.innerWidth);
+		};
+
+		window.addEventListener("resize", handleResize);
+		handleResize();
+		return () => window.removeEventListener("resize", handleResize);
+	}, []);
+
+	const logo = `< Joseph />`;
 	return (
 		<header className={headerStyles.header}>
 			<div className={headerStyles.container}>
 				<nav className={headerStyles.nav}>
-					<h4 className={headerStyles.logo}>Joseph</h4>
-					<ul className={headerStyles.nav_list}>
-						<li className={headerStyles.nav_list_item}>About</li>
-						<li className={headerStyles.nav_list_item}>Skills</li>
-						<li className={headerStyles.nav_list_item}>Projects</li>
-						<li className={headerStyles.nav_list_item}>Contact</li>
-						<li className={headerStyles.nav_list_item}>Resume</li>
-					</ul>
+					<h4 className={headerStyles.logo}>{logo}</h4>
+					{width <= 375 ? (
+						<MenuIcon />
+					) : (
+						<ul className={headerStyles.nav_list}>
+							<li className={headerStyles.nav_list_item}>About</li>
+							<li className={headerStyles.nav_list_item}>Skills</li>
+							<li className={headerStyles.nav_list_item}>Projects</li>
+							<li className={headerStyles.nav_list_item}>Contact</li>
+							<li className={headerStyles.nav_list_item}>Resume</li>
+						</ul>
+					)}
 				</nav>
 			</div>
 		</header>
