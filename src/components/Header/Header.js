fc6d8387d198cf2431dae7c9b7fce53e565d846e
Joseph Maramba <josephmaramba50@gmail.com>
2022-01-28 04:23:31 +0300

    continuing work

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,46 +1,39 @@
 import * as React from "react";
 import { useState, useEffect } from "react";
-import * as headerStyles from "./header.module.css";
-import MenuIcon from "@mui/icons-material/Menu";
+import * as styles from "./header.module.css";
+// import MenuIcon from "@mui/icons-material/Menu";
 
 const Header = () => {
 	const [width, setWidth] = useState(undefined);
 	const [isOpen, setIsOpen] = useState(false);
 
-	useEffect(() => {
-		const handleResize = () => {
-			setWidth(window.innerWidth);
-		};
+	// useEffect(() => {
+	// 	const handleResize = () => {
+	// 		setWidth(window.innerWidth);
+	// 	};
 
-		window.addEventListener("resize", handleResize);
-		handleResize();
-		return () => window.removeEventListener("resize", handleResize);
-	}, []);
+	// 	window.addEventListener("resize", handleResize);
+	// 	handleResize();
+	// 	return () => window.removeEventListener("resize", handleResize);
+	// }, []);
 
-	const handleClick = () => {
-		setIsOpen(!isOpen);
-	};
+	// const handleClick = () => {
+	// 	setIsOpen(!isOpen);
+	// };
 
-	const logo = `< Joseph />`;
+	const logo = `J.M.`;
 	return (
-		<header className={headerStyles.header}>
-			<div className={headerStyles.container}>
-				<nav className={headerStyles.nav}>
-					<h4 className={headerStyles.logo}>{logo}</h4>
-					{width <= 768 ? (
-						<MenuIcon
-							onClick={handleClick}
-							className={headerStyles.burger_icon}
-						/>
-					) : (
-						<ul className={headerStyles.nav_list}>
-							<li className={headerStyles.nav_list_item}>About</li>
-							<li className={headerStyles.nav_list_item}>Skills</li>
-							<li className={headerStyles.nav_list_item}>Projects</li>
-							<li className={headerStyles.nav_list_item}>Contact</li>
-							<li className={headerStyles.nav_list_item}>Resume</li>
-						</ul>
-					)}
+		<header className={styles.header}>
+			<div className={styles.container}>
+				<nav className={styles.nav}>
+					<h4 className={styles.logo}>{logo}</h4>
+					<ul className={styles.nav_list}>
+						<li className={styles.nav_list_item}>About</li>
+						<li className={styles.nav_list_item}>Skills</li>
+						<li className={styles.nav_list_item}>Projects</li>
+						<li className={styles.nav_list_item}>Contact</li>
+						<li className={styles.nav_list_item}>Resume</li>
+					</ul>
 				</nav>
 			</div>
 		</header>
